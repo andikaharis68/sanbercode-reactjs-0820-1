db3d11463c397c6c88f255c46fff9b46d664b21d
Andika Haris <andikaharis68@gmail.com>
2020-08-27 22:47:09 +0700

    tugas4 selesai

---
+++ Tugas-4/tugas4.js
@@ -0,0 +1,45 @@
+/*============= SOAL 1 ================*/
+console.log("LOOPING PERTAMA")
+var deret1 = 0;
+while (deret1 < 20) {
+    deret1 += 2
+    console.log(deret1 + " - I love coding")
+}
+console.log("LOOPING KEDUA")
+deret2 = 20;
+while (deret2 > 0) {
+    console.log(deret2 + " - I will become a frontend developer")
+    deret2 -= 2
+}
+
+/*============= SOAL 2 ================*/
+for (var angka = 1; angka <= 20; angka++) {
+    if (angka % 2 == 1 && angka % 3 == 0) {
+        console.log(angka + " - I Love Coding ")
+    } else if (angka % 2 == 1) {
+        console.log(angka + " - Berkualitas ")
+    } else {
+        console.log(angka + " - Santai ")
+    }
+}
+
+/*============= SOAL 3 ================*/
+for (var i = 1; i <= 7; i++) {
+    var x = []
+    for (var j = 0; j < i; j++) {
+        x.push("#")
+    }
+    console.log(x.join(""))
+}
+
+/*============= SOAL 4 ================*/
+var kalimat = "saya sangat senang belajar javascript"
+console.log(kalimat.split(" "))
+
+/*============= SOAL 5 ================*/
+var daftarBuah = ["2. Apel", "5. Jeruk", "3. Anggur", "4. Semangka", "1. Mangga"];
+daftarBuah = daftarBuah.sort()
+for (var i = 0; i < daftarBuah.length; i++) {
+    x = daftarBuah.slice(i, i + 1)
+    console.log(x.join())
+}
